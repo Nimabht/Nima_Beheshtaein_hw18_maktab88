67bd6b75ba58a4b95ccd1e3e5a2faf952981df2a
nimabht <nimabeheshtaein99@gmail.com>
2023-04-17 15:20:34 +0330

    user joi validator added

---
+++ validators/user.js
@@ -0,0 +1,73 @@
+import Joi from "joi";
+
+export default (user) => {
+  const schema = Joi.object({
+    firstname: Joi.string().min(2).max(50).required().messages({
+      "string.base": "First name must be a string",
+      "string.empty": "First name cannot be empty",
+      "string.min":
+        "First name must be at least {#limit} characters long",
+      "string.max":
+        "First name must be at most {#limit} characters long",
+      "any.required": "First name is required",
+    }),
+    lastname: Joi.string().min(2).max(50).required().messages({
+      "string.base": "Last name must be a string",
+      "string.empty": "Last name cannot be empty",
+      "string.min":
+        "Last name must be at least {#limit} characters long",
+      "string.max":
+        "Last name must be at most {#limit} characters long",
+      "any.required": "Last name is required",
+    }),
+    username: Joi.string()
+      .min(4)
+      .max(255)
+      .lowercase()
+      .required()
+      .messages({
+        "string.base": "Username must be a string",
+        "string.empty": "Username cannot be empty",
+        "string.min":
+          "Username must be at least {#limit} characters long",
+        "string.max":
+          "Username must be at most {#limit} characters long",
+        "any.required": "Username is required",
+      }),
+    password: Joi.string()
+      .min(8)
+      .max(255)
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required()
+      .messages({
+        "string.base": "Password must be a string",
+        "string.empty": "Password cannot be empty",
+        "string.pattern.base":
+          "Password must be at least 8 characters long and contain only letters and digits",
+        "string.min":
+          "Password must be at least {#limit} characters long",
+        "string.max":
+          "Password must be at most {#limit} characters long",
+        "any.required": "Password is required",
+      }),
+    repeat_password: Joi.ref("password").messages({
+      "any.only": "Passwords must match",
+    }),
+    gender: Joi.string().min(3).max(50).default("not-set").messages({
+      "string.base": "Gender must be a string",
+      "string.min":
+        "Gender must be at least {#limit} characters long",
+      "string.max": "Gender must be at most {#limit} characters long",
+    }),
+    role: Joi.string()
+      .valid("user", "admin")
+      .default("user")
+      .messages({
+        "any.only": 'Role must be "user" or "admin"',
+      }),
+    registrationDate: Joi.date().forbidden().messages({
+      "any.unknown": "Registration date cannot be set",
+    }),
+  });
+  return schema.validate(user, { abortEarly: false });
+};
