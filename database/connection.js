8903a83dd75427ec35dd5d4fc375e0de1d30aa45
nimabht <nimabeheshtaein99@gmail.com>
2023-04-17 23:57:03 +0330

    mongoose connection updated (options)

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 
 mongoose
-  .connect("mongodb://localhost:27017/HW18")
+  .connect("mongodb://localhost:27017/HW18", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB connected!");
   })
