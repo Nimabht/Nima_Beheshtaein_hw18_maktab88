0145d427d785b8f6493f1e063b2bd068b8c12cef
nimabht <nimabeheshtaein99@gmail.com>
2023-04-17 15:20:04 +0330

    user model updated

--- models/user.js
+++ models/user.js
@@ -1,33 +1,52 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-const userSchema = mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50,
+const userSchema = mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    lastname: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    username: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 255,
+      uniqe: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      maxlength: 1024,
+    },
+    gender: {
+      type: String,
+      minlength: 3,
+      maxlength: 50,
+      default: "not-set",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  lastname: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 50,
-  },
-  username: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 255,
-    uniqe: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 1024,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "registrationDate",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
 userSchema.methods.validatePassword = async function validatePassword(
   password
