ce73e2987f658d47367977bf6c36681fb28c4c72
nimabht <nimabeheshtaein99@gmail.com>
2023-04-17 23:56:08 +0330

    session added to proj

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import db from "./database/connection.js";
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
-
+import user from "./routes/user.js";
+import session from "express-session";
 const app = express();
 
 app.use(express.static("public"));
 app.set("views", "./views");
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.MY_SECRET_KEY,
+    resave: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    saveUninitialized: false,
+  })
+);
+
+app.use("/api/user", user);
 
 app.use(globalErrorHandler);
 
